ed3a0281232445abaf82b56b76d2ec573ae4be05
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-01 19:38:03 -0300

    Real-time with socket.io client for mobile

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Routes from './src/routes'
+import { YellowBox } from 'reac-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return <Routes />
